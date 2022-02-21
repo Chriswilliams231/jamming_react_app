562c7686335cceb3fa339be035d0e5650b0a745a
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-02-20 19:18:05 -0800

    adding props to each file

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,8 +13,25 @@ class App extends React.Component {
         { name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
         { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
         { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
+      ],
+      playlistName: 'Migos',
+      playlistTracks: [
+        { name: 'playListName1', artist: 'playListArtist1', album: 'playListAlbum1', id: 1 },
+        { name: 'playListName2', artist: 'playListArtist2', album: 'playListAlbum2', id: 2 },
+        { name: 'playListName3', artist: 'playListArtist3', album: 'playListAlbum3', id: 3 }
       ]
     }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === tracks.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks })
   }
   render() {
     return (
@@ -24,8 +41,8 @@ class App extends React.Component {
 
           <SearchBar />
           <div className="App-playlist">
-            <SearchBarResults searchResults={this.state.searchResults} />
-            <PlayList />
+            <SearchBarResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
